395c47a0fb1312c823f5c08d7ceff271297c1977
ramy badr <eng.ramybadr66@gmail.com>
2023-06-05 15:06:18 +0300

    setup swagger

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { AuthenticationService } from './authentication.service';
 import { Auth } from './decorators/auth.decorator';
 import { RefreshTokenDto } from './dto/refresh-token.dto';
@@ -6,6 +7,7 @@ import { SignInDto } from './dto/sign-in.dto';
 import { SignUpDto } from './dto/sign-up.dto';
 import { AuthType } from './enums/auth-type.enum';
 
+@ApiTags('Authentication')
 @Auth(AuthType.None)
 @Controller('authentication')
 export class AuthenticationController {
